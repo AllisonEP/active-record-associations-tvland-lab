63fcaefcdc90f5efa1af6bab296dd7b6a35e27cb
Allison Perry <allisonpshortnorth@gmail.com>
2020-12-21 23:13:22 -0500

    commit tree a9d707355b0b190a33b5f3ba45df4217fc9c4683

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+        self.map do |character|
+            "#{character.name} always says: #{character.catchphrase}"
+        end
+  end
 end
